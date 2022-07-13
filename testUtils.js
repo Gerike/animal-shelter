229d75f0699cfa11ec2ffc33a980b794e315ef66
Kovács Gergő <Gerike1995@gmail.com>
2022-07-13 23:20:02 +0200

    fixes "lint"

--- testUtils.js
+++ testUtils.js
@@ -4,9 +4,9 @@ const insertTestType = async (db, type) => {
 
 const insertTestAnimal = async (db, { name, type, color, age, treated }) => {
     await db.query({ text: 'INSERT INTO animal (name, type, color, age, treated) VALUES ($1, $2, $3, $4, $5)', values: [name, type, color, age, treated] });
-}
+};
 
 const clearDatabase = async(db) => {
     await db.query('TRUNCATE animals');
     await db.query('TRUNCATE types');
-}
+};
